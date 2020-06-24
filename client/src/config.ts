9d3f25e4245e0701ec84cb976627b6e6ef586f4f
Eshan Jain <eshanja1n@users.noreply.github.com>
2020-06-24 14:14:42 -0700

    updated

--- client/src/config.ts
+++ client/src/config.ts
@@ -8,7 +8,7 @@ export const authConfig = {
 // TODO: Create an Auth0 application and copy values from it into this map
 
 
-  domain: 'dev-kcsqy-p9.us.auth0.com',    // Auth0 domain
-  clientId: 's9J2mMpZzuegRuc5rJuDxWlZCsmszuAb',      // Auth0 client id
+  domain: 'dev-xa9u920z.auth0.com',    // Auth0 domain
+  clientId: '46g7LYL8gq42DQEBADiAEuL6yR895x9R',      // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
